2d2fce60c54f7cbc6d2ca733f6b5aba7b2a6572e
nwent-dev <nwent@proton.me>
2025-02-08 10:18:14 +0600

    Initial Commit

---
+++ Aimly/AimlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimlyApp.swift
+//  Aimly
+//
+//  Created by Александр Калашников on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AimlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aimly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aimly
+//
+//  Created by Александр Калашников on 08.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
